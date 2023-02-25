41b1eb9d02b17abc49a5fcaad225ef4e43cd7246
Chaitanya Waghmare <53860781+chai04@users.noreply.github.com>
2023-02-25 20:33:43 +0530

    Add files via upload

---
+++ AddSubMulDiv/AddSubMulDiv.js
@@ -0,0 +1,9 @@
+let a, b, c;
+
+a = parseInt(prompt("Enter a number"));
+b = parseInt(prompt("Enter a number"));
+c = a + b;
+document.write("Addition is :" + c + "<br>");
+document.write("Subtraction is :" + (a-b)+ "<br>");
+document.write("Multiplication is :" + (a*b)+ "<br>");
+document.write("Division is :" + (a/b)+ "<br>");
